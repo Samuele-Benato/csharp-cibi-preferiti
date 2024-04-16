ad8920ea6ead9ca65a0404845f74119deb4d5b5c
Samuele-Benato <samuele.benato@gmail.com>
2024-04-16 16:46:24 +0200

    feat : add exercise

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] favoriteFood = { "Gelato", "Sushi", "Pizza", "Pasta", "Hamburger" };
+
+            Console.WriteLine($"Lunghezza della classifica: {favoriteFood.Length}");
+
+            Console.WriteLine("La mia classifica:");
+
+            for (int i = 0; i < favoriteFood.Length; i++)
+            {
+                Console.WriteLine($"{(i + 1)} - {favoriteFood[i]}");
+            }
+
+            Console.WriteLine($"Il mio cibo top:{favoriteFood[0]}");
+
+            Console.WriteLine($"Il mio cibo meno preferito: {favoriteFood[favoriteFood.Length - 1]}");
         }
     }
 }
